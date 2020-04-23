0103c77deebecee9cdbc51a635e7571ea206af30
rodrigo sanchez peregrino <rodi1828@hotmail.com>
2020-04-23 14:32:02 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## aplicacion de comando
+
+este es el ejercicio del curso
+
+Recuerden instalar los paquetes de node asi
+
+```
+npm install
+```
---
+++ config/yargs.js
@@ -0,0 +1,32 @@
+
+
+const descripcion = {
+  demand:true,
+  alias: 'd',
+  desc: 'Descripcion actualiza el estado de una tarea'
+};
+
+const completado = {
+  default: true,
+  alias: 'c',
+  desc: 'marca como completado o pendiente la tarea'
+};
+
+
+const argv = require('yargs')
+              .command('crear','Crear un elemento por hacer',{
+                descripcion
+              })
+              .command('actualizar','actualiza el estado completado de una tarea',{
+                descripcion,
+                completado
+              })
+              .command('borrar','borra una tarea',{
+                descripcion
+              })
+              .help()
+              .argv;
+
+module.exports = {
+  argv
+};
